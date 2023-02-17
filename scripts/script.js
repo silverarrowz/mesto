e53313eabce6f430e6b2b27259074b86f478eafb
Dian4ik <jakie16@yandex.ru>
2023-02-17 12:40:09 +0800

    renamed variables & classes

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-const addBtn = document.querySelector('.profile__add-button');
-const editBtn = document.querySelector('.profile__edit-button');
+const btnPopupCreateCard = document.querySelector('.profile__add-button');
+const btnPopupEditProfile = document.querySelector('.profile__edit-button');
 const popups = document.querySelectorAll('.popup');
 
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-const formEdit = document.querySelector('.form_type_edit-profile'); 
+const formEditProfile = document.querySelector('.form_type_edit-profile'); 
 const profileName = document.querySelector('.profile__name');
-const profileTitle = document.querySelector('.profile__title');
+const profileAbout = document.querySelector('.profile__about');
 const nameInput = document.querySelector('.form__field_user_name');
-const titleInput = document.querySelector('.form__field_user_title');
+const aboutInput = document.querySelector('.form__field_user_about');
 
 const popupCreateCard = document.querySelector('.popup_type_create-card');
 const formCreateCard = document.querySelector('.form_type_create-card');
@@ -68,17 +68,17 @@ function closeOnEsc() {
 function openPopupEditProfile() {
   openPopup(popupEditProfile);
   nameInput.value = profileName.textContent;
-  titleInput.value = profileTitle.textContent;
+  aboutInput.value = profileAbout.textContent;
 }
 
 function profileFormSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
-  profileTitle.textContent = titleInput.value;
+  profileAbout.textContent = aboutInput.value;
   closePopup(popupEditProfile);
 }
 
-function addCard(name, link) {
+function createCard(name, link) {
   const cardElement = cardTemplate.cloneNode(true);
   cardElement.querySelector('.element__title').textContent = name;
   cardElement.querySelector('.element__image').alt = name;
@@ -105,21 +105,21 @@ function addCard(name, link) {
 
 function renderCards() {
   const cards = initialCards.map((item) => {
-    return addCard(item.name, item.link);
+    return createCard(item.name, item.link);
   });
   elements.append(...cards);
 }
 
-editBtn.addEventListener('click', openPopupEditProfile);
-formEdit.addEventListener('submit', profileFormSubmitHandler);
+btnPopupEditProfile.addEventListener('click', openPopupEditProfile);
+formEditProfile.addEventListener('submit', profileFormSubmitHandler);
 
-addBtn.addEventListener('click', function () {
+btnPopupCreateCard.addEventListener('click', function () {
   openPopup(popupCreateCard);
 });
 
 formCreateCard.addEventListener('submit', function(evt) {
   evt.preventDefault();
-  elements.prepend(addCard(cardNameInput.value, cardPictureInput.value));
+  elements.prepend(createCard(cardNameInput.value, cardPictureInput.value));
   evt.target.reset();
   closePopup(popupCreateCard);
 });
